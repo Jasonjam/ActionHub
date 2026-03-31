b44e40eb64355cf22198fa9586130b77bcf8846d
Jasonjam <jasonjam66@gmail.com>
2026-03-31 14:38:45 +0800

    fix: 修正 bat 輸出 exe 位置的問題

--- build.bat
+++ build.bat
@@ -9,13 +9,23 @@ set "V1_ENGINE=tools\AHK_V1_Engine.exe"
 set "COMPILER=tools\Ahk2Exe\Ahk2Exe.ahk"
 set "V2_BASE=tools\AutoHotkey64_v2.exe"
 set "ICON=assets\app_icon.ico"
-set "OUT_EXE=ActionHub.exe"
-set "SOURCE=main.ahk2"
+set "OUT_EXE=.\ActionHub.exe"
+set "SOURCE=src\main.ahk2"
 
-echo [1/2] 找尋並清理舊exe檔案...
+echo [1/2] 找尋並清理舊的 exe 檔案...
+echo       Search old version exe and delete it.
+echo.
 if exist "%OUT_EXE%" del "%OUT_EXE%"
 
-echo [2/2] 正在進行靜默編譯...，黃色Warning，按下確定即可
+:: 延遲1秒
+timeout /t 1 /nobreak >nul
+
+echo [2/2] 正在進行靜默編譯...，黃色 Warning，按下確定即可
+echo       Compiling... If yellow warnings appear, click OK.
+echo.
+
+:: 延遲1秒
+timeout /t 1 /nobreak >nul
 
 :: 使用引號包覆所有路徑，並確保參數順序正確
 "%V1_ENGINE%" "%COMPILER%" /in "%SOURCE%" /out "%OUT_EXE%" /icon "%ICON%" /base "%V2_BASE%"
@@ -23,10 +33,14 @@ echo [2/2] 正在進行靜默編譯...，黃色Warning，按下確定即可
 if %ERRORLEVEL% EQU 0 (
     echo.
     echo ========================================
-    echo   編譯成功！產出檔案: ActionHub.exe
+    echo   編譯成功! 產出檔案: ActionHub.exe
+    echo   Success! Created: ActionHub.exe
     echo ========================================
+    echo.
+    echo 按下任何鍵可關閉
 ) else (
     echo.
     echo [錯誤] 編譯失敗，錯誤代碼: %ERRORLEVEL%
+    echo [Error] Compilation failed. Code: %ERRORLEVEL%
 )
 pause
